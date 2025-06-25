75d288d8d1eb591ebb9ceddf5f7b8849b0f01b0d
Théo <t.knopfer@gmail.com>
2025-06-25 15:22:22 +0200

    gallery v0.7

--- src/Controller/GallerieController.php
+++ src/Controller/GallerieController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PicturesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class GallerieController extends AbstractController
 {
     #[Route('/gallerie', name: 'gallerie')]
-    public function index(): Response
+    public function index(PicturesRepository $repository): Response
     {
+        
+        $pictures = $repository->findAll();
         return $this->render('gallerie/gallerie.html.twig', [
+        'pictures'=>$pictures,
         ]);
     }
 }
