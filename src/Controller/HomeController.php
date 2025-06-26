dc1b113fecfc3fa4d0db5431d41e11f64e8247b1
Théo <t.knopfer@gmail.com>
2025-06-26 13:47:41 +0200

    4derniers sur accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PicturesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function findLatest(PicturesRepository $picturesRepo): Response
     {
+    $latestFour = $picturesRepo->findBy(
+        [],
+        ['id' => 'DESC'],
+        4                          
+    );
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+            'latests' => $latestFour,
         ]);
     }
 }
