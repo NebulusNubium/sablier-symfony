1c7318dece8a241b987534fa6acf0f97ae804fc0
Théo <t.knopfer@gmail.com>
2025-06-27 12:56:21 +0200

    contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,17 +1,47 @@
 <?php
-
+// src/Controller/ContactController.php
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\{
+    Request,
+    Response
+};
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
-final class ContactController extends AbstractController
+class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response 
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            // on envoie un email
+            $email = (new Email())
+                ->from($data['email'])
+                ->to('toto@gmail.com')
+                ->subject('Nouveau message de contact')
+                ->text(
+                    "Nom: {$data['name']}\n".
+                    "Email: {$data['email']}\n\n".
+                    "Message:\n{$data['message']}"
+                );
+            $mailer->send($email);
+
+            // flash & redirection
+            $this->addFlash('success', 'Votre message a bien été envoyé.');
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('contact/contact.html.twig', [
+            'contactForm' => $form->createView(),
         ]);
     }
 }
